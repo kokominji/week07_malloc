665b8257d8d494cd448ae000d2eab9bac1a97284
kokominji <kmj6386@gmail.com>
2025-06-30 15:13:47 +0900

    #define

--- malloc-lab/mm.c
+++ malloc-lab/mm.c
@@ -18,6 +18,33 @@
 #include "mm.h"
 #include "memlib.h"
 
+//상수/매크로 정의
+#define WSIZE 4 
+#define DSIZE 8
+#define CHUNKSIZE (1<<12)
+
+//최소 블록 크기 보장
+#define MAX(x,y) ((x) > (y)? (x) : (y)) //요청 크기가 너무 작다면 최소 블록 크기로 할당함
+
+//하나의 헤더/푸터 = 블록 크기 (size) + 할당여부 (alloc)
+#define PACK(size, alloc) ((size) | (alloc))
+
+//주소 p에서 4바이트 워드를 읽고 씀
+#define GET(p) (*(unsigned int *)(p))
+#define PUT(p, val) (*(unsigned int *)(p) = (val))
+
+//헤더/푸터에 저장된 정보에서 블록의 크기, 할당 여부를 분리해서 읽어오기
+#define GET_SIZE(p) (GET(p) & ~0x7) //블록의 크기만 가지고 옴
+#define GET_ALLOC(p) (GET(p) & 0x1) //블록의 할당 여부만 가지고 옴
+
+//헤더와 푸터의 위치를 가리키는 포인터
+#define HDRP(bp) ((char *)(bp) - WSIZE) //헤더는 푸터보다 4바이트 앞
+#define FTRP(bp) ((char *)(bp) + GET_SIZE(HDRP(bp)) - DSIZE)
+
+//다음 블록의 시작주소, 이전 블록의 시작 주소 계산
+#define NEXT_BLKP(bp) ((char *)(bp) + GET_SIZE(((char *)(bp) - WSIZE))) //bp에서 현재 블록 크기만큼 더함
+#define PREV_BLKP(bp) ((char *)(bp) - GET_SIZE(((char *)(bp) - DSIZE))) //bp에서 이전 블록의 크기만큼 뺌
+
 /*********************************************************
  * NOTE TO STUDENTS: Before you do anything else, please
  * provide your team information in the following struct.
@@ -26,11 +53,11 @@ team_t team = {
     /* Team name */
     "ateam",
     /* First member's full name */
-    "Harry Bovik",
+    "고민지",
     /* First member's email address */
-    "bovik@cs.cmu.edu",
+    "kmj6386@gmail.com",
     /* Second member's full name (leave blank if none) */
-    "",
+    "이지윤",
     /* Second member's email address (leave blank if none) */
     ""};
 
